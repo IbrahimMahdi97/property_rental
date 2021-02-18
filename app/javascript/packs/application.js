4331c45a005088e8e52fab437af3fe98662d6964
IbrahimMahdi97 <ibrahimmahdi115@gmail.com>
2021-02-19 01:13:34 +0300

    add google maps to the background and fix styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,12 @@ import "channels";
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+
+$(document).ready(function () {
+  $("div.cards-cont .room-card").click(function () {
+    $(".room-card").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
+
