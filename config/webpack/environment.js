0a5366737874efa8e6190054589e8a6fb8099294
IbrahimMahdi97 <ibrahimmahdi115@gmail.com>
2021-02-24 02:12:31 +0300

    fix jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,30 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+const webpack = require("webpack");
+
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery/src/jquery",
+    jQuery: "jquery/src/jquery",
+    jquery: "jquery",
+    "window.jQuery": "jquery",
+    Popper: ["popper.js", "default"],
+  })
+);
+
+// environment.loaders.append("jquery", {
+//   test: require.resolve("jquery"),
+//   use: [
+//     {
+//       loader: "expose-loader",
+//       options: "$",
+//     },
+//     {
+//       loader: "expose-loader",
+//       options: "jQuery",
+//     },
+//   ],
+// });
+
+module.exports = environment;
