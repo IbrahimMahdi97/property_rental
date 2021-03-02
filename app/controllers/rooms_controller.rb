dd077adde7db55dd71aced0ea461dbacb0e7796b
IbrahimMahdi97 <ibrahimmahdi115@gmail.com>
2021-03-02 23:29:43 +0300

    make the  map function onclick

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -69,6 +69,6 @@ class RoomsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def room_params
-      params.require(:room).permit(:title, :description, :image, :available_at, :address_line1, :price, :longitude, :latitude)
+      params.require(:room).permit(:title, :description, :image, :available_at, :address_line1, :price, :longitude, :latitude, :id)
     end
 end
