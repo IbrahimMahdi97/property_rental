8b2683d4ac6b66b39e07d15f0c02aa71179bffee
IbrahimMahdi97 <ibrahimmahdi115@gmail.com>
2021-03-09 00:35:10 +0300

    add image url option to _form and _room

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_173357) do
+ActiveRecord::Schema.define(version: 2021_03_08_203943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_173357) do
     t.decimal "price", precision: 8, scale: 2
     t.decimal "longitude", precision: 12, scale: 8
     t.decimal "latitude", precision: 12, scale: 8
+    t.string "image_url"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
