e618af33bedeb9dee340a8827c0cd4e87c2ad4da
IbrahimMahdi97 <ibrahimmahdi115@gmail.com>
2021-03-09 22:54:34 +0300

    adding image helper

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,10 @@
 module RoomsHelper
+  def image_pat(room)
+    if room.image.present?
+      image_tag(room.image, alt: 'Property Image', class:"img_preview")
+    elsif room.image_url.present?
+      image_tag(room.image_url, alt: 'Property Image', class:"img_preview")
+    else 
+    end
+  end
 end
