9164635daf8213302eba25128d39636a730fcf3e
IbrahimMahdi97 <ibrahimmahdi115@gmail.com>
2021-03-15 22:17:34 +0300

    pass add new room with in-valid attributes test

--- spec/features/new_room_spec.rb
+++ spec/features/new_room_spec.rb
@@ -5,40 +5,23 @@ require 'capybara/rspec'
 RSpec.describe Room, type: :model do
   feature "Add new Room" do
     scenario 'with valid attributes' do
-      new_room_with('address', 'title') do
+      new_room_with('NY', 'Bedroom 2C') do
         expect(room).to be_valid
       end
     end
+
+    scenario 'with in-valid attributes' do
+      new_room_with('', '') do
+        expect(room).to_not be_valid
+      end
+    end
   end
-    # it "has an address" do
-    #   room = Room.new(
-    #     title: "Bedroom 1A",
-    #     address_line1: ""
-    #   )
-    #   expect(room).to_not be_valid
-
-    #   room.address_line1 = "NY"
-    #   expect(room).to be_valid
-    # end
-
-    # it "has a title" do
-    #   room = Room.new(
-    #     title: "",
-    #     address_line1: "MI"
-    #   )
-    #   expect(room).to_not be_valid
-
-    #   room.title = "Bedroom 3C"
-    #   expect(room).to be_valid
-    # end
-
+  
   def new_room_with(address, title)
     visit '/rooms/new'
     
-    binding.pry
-    
-    fill_in "room[address_line1]",	with: "New York" 
-    fill_in "room[title]",	with: "Beddrom 3C"
+    fill_in "room[address_line1]",	with: address
+    fill_in "room[title]",	with: title
     click_button "Create Room"
   end
 end
